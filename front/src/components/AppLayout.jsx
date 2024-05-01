3170a3a98cd03a179af531a8b112c0dcd88bcfef
Дмитрий Чанков <chankovbelka123@yandex.ru>
2024-05-02 00:23:03 +0400

    update

--- front/src/components/AppLayout.jsx
+++ front/src/components/AppLayout.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Layout, Flex } from 'antd';
-import { FileAddTwoTone } from '@ant-design/icons'
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, Link } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
-const { Header, Content, Footer } = Layout;
+import XMLIcon from "../assets/xml-svgrepo-com.svg?react"
+const { Header, Content } = Layout;
 
 const navLinks = [
   {key: 0, label: 'Главная', link: '/'},
@@ -25,9 +25,13 @@ export const AppLayout = () => {
   >
     <Layout style={{minHeight: "100%"}}>
       <Header>
-          <Flex gap={'middle'}>
-            <h1 style={{color: 'green'}}>SAPRXML</h1>
-            <FileAddTwoTone twoToneColor="green" style={{ fontSize: '32px'}} />
+          <Flex gap={'middle'} >
+            <Link to="/">
+              <Flex gap={'middle'} align='center'>
+                <h1 style={{color: 'green'}}>SAPRXML</h1>
+                <XMLIcon width={32} height={32} />
+              </Flex>         
+            </Link>
             {navLinks.map((el) => <NavLink className='nav-link' key={el.key} to={el.link}>{el.label}</NavLink>)}
           </Flex>
       </Header>
